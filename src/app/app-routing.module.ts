77362a882aa585b7bada548909e3d3c7890ca137
Mariano Fernandez <mariano.b.fernandez@gmail.com>
2021-07-10 18:52:28 -0300

    basic routes & update product component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrdersComponent } from './components/orders/orders.component';
+import { ProductsComponent } from './components/products/products.component';
+import { UsersComponent } from './components/users/users.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pedidos', component: OrdersComponent },
+  { path: 'productos', component: ProductsComponent },
+  { path: 'usuarios', component: UsersComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
