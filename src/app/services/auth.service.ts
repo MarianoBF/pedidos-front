808d2735810967b1d8fe8460e02d365d71be0fcf
Mariano Fernandez <mariano.b.fernandez@gmail.com>
2021-07-11 20:58:36 -0300

    update environment  & existing services

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,13 @@
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { tap } from 'rxjs/operators'
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  apiUrl = 'http://localhost:8500/api/v1';
+  apiUrl = environment.apiURL;
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
@@ -21,6 +22,6 @@ export class AuthService {
     }
     return this.http
       .post<any>(this.apiUrl+"/usuario/login", credentials, this.httpOptions)
-      .pipe(tap((user) => console.log('Login attemp for', user)));
+      .pipe(tap(_ => console.log('Login attemp for', values.userName)));
   }
 }
