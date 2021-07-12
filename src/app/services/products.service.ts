0af4f1eacc8b8955e6b12fc60a9e90978866bbfc
Mariano Fernandez <mariano.b.fernandez@gmail.com>
2021-07-12 20:53:02 -0300

    included products interface, updated service

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { Product } from '../common/interfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -13,19 +14,25 @@ export class ProductsService {
 
   getProducts() {
     return this.http
-      .get<any[]>(this.apiURL + 'productos')
+      .get<Product[]>(this.apiURL + 'productos')
       .pipe(tap((prods) => console.log('retrieved products', prods)));
   }
 
-  // addProduct(){
+  addProduct(product: Product) {
+    return this.http
+      .post<any>(this.apiURL + 'productos/', product)
+      .pipe(tap((prods) => console.log('retrieved products', prods)));
+  }
 
-  // }
+  updateProduct(id: number, product: Product) {
+    return this.http
+      .put<any>(this.apiURL + 'productos/' + id, product)
+      .pipe(tap((prods) => console.log('retrieved products', prods)));
+  }
 
-  // updateProduct(){
-
-  // }
-
-  // deleteProduct(){
-
-  // }
+  deleteProduct(id: number) {
+    return this.http
+      .delete<any>(this.apiURL + 'productos/' + id)
+      .pipe(tap((prods) => console.log('retrieved products', prods)));
+  }
 }
