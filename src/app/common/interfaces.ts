50d2561916182a8e46620a0de829863304268b5e
Mariano Fernandez <mariano.b.fernandez@gmail.com>
2021-07-14 22:32:44 -0300

    update orders service

--- src/app/common/interfaces.ts
+++ src/app/common/interfaces.ts
@@ -4,3 +4,13 @@ export interface Product {
     descripcion: string,
     precio: number,
 }
+
+export interface Order {
+    id_orden?: number,
+    hora: Date,
+    id_usuario: number,
+    estado: string,
+    pago_via: string,
+    pago_monto: number,
+    observaciones?: string,
+}
--- src/app/services/orders.service.ts
+++ src/app/services/orders.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { tap } from 'rxjs/operators';
+import { Order } from '../common/interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrdersService {
+  apiURL = environment.apiURL;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getOrders() {
+    return this.http
+      .get<Order[]>(this.apiURL + 'pedidos')
+      .pipe(tap((prods) => console.log('retrieved products', prods)));
+  }
+
+  addOrder(product: Order) {
+    return this.http
+      .post<any>(this.apiURL + 'pedido/', product)
+      .pipe(tap((order) => console.log('retrieved products', order)));
+  }
+
+  updateOrder(id: number, product: Order) {
+    return this.http
+      .put<any>(this.apiURL + 'pedido/' + id, product)
+      .pipe(tap((order) => console.log('retrieved products', order)));
+  }
+
+  deleteOrder(id: number) {
+    return this.http
+      .delete<any>(this.apiURL + 'pedido/' + id)
+      .pipe(tap((order) => console.log('retrieved products', order)));
+  }
 }
