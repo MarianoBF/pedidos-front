b5099635e1ce78c389208eb14baa074abeb80e85
Mariano Fernandez <mariano.b.fernandez@gmail.com>
2021-07-14 22:30:10 -0300

    updated product service

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -21,18 +21,18 @@ export class ProductsService {
   addProduct(product: Product) {
     return this.http
       .post<any>(this.apiURL + 'producto/', product)
-      .pipe(tap((prods) => console.log('retrieved products', prods)));
+      .pipe(tap((prods) => console.log('added product', prods)));
   }
 
   updateProduct(id: number, product: Product) {
     return this.http
       .put<any>(this.apiURL + 'producto/' + id, product)
-      .pipe(tap((prods) => console.log('retrieved products', prods)));
+      .pipe(tap((prods) => console.log('update product', prods)));
   }
 
   deleteProduct(id: number) {
     return this.http
       .delete<any>(this.apiURL + 'producto/' + id)
-      .pipe(tap((prods) => console.log('retrieved products', prods)));
+      .pipe(tap((prods) => console.log('deleted product', prods, "with id", id)));
   }
 }
