329ddabe7864926967d1b7c261fc94f10458bcb8
Mariano <mariano.b.fernandez@gmail.com>
2021-07-20 10:52:44 -0300

    include authguard & update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, CanActivate } from '@angular/router';
 import { OrdersComponent } from './components/orders/orders.component';
 import { ProductsComponent } from './components/products/products.component';
 import { UsersComponent } from './components/users/users.component';
-import { AppComponent } from './app.component';
+import { AuthGuard } from './auth.guard';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: 'pedidos', component: OrdersComponent },
-  { path: 'productos', component: ProductsComponent },
-  { path: 'usuarios', component: UsersComponent },
+  { path: 'pedidos', component: OrdersComponent, canActivate: [AuthGuard] },
+  { path: 'productos', component: ProductsComponent, canActivate: [AuthGuard] },
+  { path: 'usuarios', component: UsersComponent, canActivate: [AuthGuard] },
+  { path: '', component: LoginComponent},
+  { path: '**', redirectTo: '/' }
 ];
 
 @NgModule({
