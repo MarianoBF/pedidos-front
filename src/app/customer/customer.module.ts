713f624e1b9ae0559511f7334357fc1559bd089b
Mariano <mariano.b.fernandez@gmail.com>
2021-07-26 21:05:38 -0300

    addorder - vieworder components

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddOrderComponent } from './components/add-order/add-order.component';
+import { ViewOrdersComponent } from './components/view-orders/view-orders.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddOrderComponent,
+    ViewOrdersComponent
+  ],
   imports: [
     CommonModule
   ]
