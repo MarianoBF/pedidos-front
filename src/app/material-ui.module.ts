d551fb38e614de0a534ea4779b7088600e6fb4aa
Mariano <mariano.b.fernandez@gmail.com>
2021-07-29 12:11:25 -0300

    updated login component

--- src/app/material-ui.module.ts
+++ src/app/material-ui.module.ts
@@ -1,19 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
+import { MatGridListModule } from '@angular/material/grid-list';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    MatToolbarModule,
-    MatButtonModule,
-  ],
-  exports: [
-    MatToolbarModule,
-    MatButtonModule,
-  ]
+  exports: [MatButtonModule, MatGridListModule, MatInputModule, MatToolbarModule, ],
 })
-export class MaterialUIModule { }
+export class MaterialUIModule {}
