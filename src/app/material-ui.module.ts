c50f2a86550b97b4894a5c231f8a76b2271504fd
Mariano <mariano.b.fernandez@gmail.com>
2021-08-06 12:15:52 -0300

    changed navbar to sidebar

--- src/app/material-ui.module.ts
+++ src/app/material-ui.module.ts
@@ -2,16 +2,28 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatGridListModule } from '@angular/material/grid-list';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  exports: [MatButtonModule, MatGridListModule, MatInputModule, MatToolbarModule,  MatCardModule,
+  exports: [
+    MatButtonModule,
+    MatGridListModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatCardModule,
     MatFormFieldModule,
     MatTableModule,
-    MatSnackBarModule, ],
+    MatSnackBarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+  ],
 })
 export class MaterialUIModule {}
