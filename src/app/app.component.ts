37ccba212d5a2e59453adfe5e2e5948d3c6c0216
Mariano <mariano.b.fernandez@gmail.com>
2021-08-13 18:14:55 -0300

    adjust list & dialogue, fix name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './services/auth.service';
 import { Router } from '@angular/router';
 import { loggedInUser } from './common/interfaces';
@@ -8,11 +8,14 @@ import { loggedInUser } from './common/interfaces';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   user?: loggedInUser = this.authService.userData;
 
   constructor (private authService: AuthService, private router: Router) {}
 
+  ngOnInit() {
+    this.user = this.authService.userData;
+  }
 
   logout() {
     console.log("logout", "logged", this.authService.logged);
