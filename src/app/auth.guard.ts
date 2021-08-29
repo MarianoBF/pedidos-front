5adb2d288b6c2c2985e82c054974a811a8aa65a6
Mariano <mariano.b.fernandez@gmail.com>
2021-08-29 12:57:35 -0300

    update guard

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -13,38 +13,30 @@ export class AuthGuard implements CanActivate {
 
   constructor(private authService: AuthService) { }
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    //Check if token is current, 
-    // const storedToken = (JSON.parse(localStorage.getItem('pedidos456') || '[]') || "")
+  canActivate(): any {
     const userData = this.authService.userData
-    if (userData.token) {
-      const decodedStoredToken: decodedToken = jwt_decode(userData.token)
-      if (decodedStoredToken.exp >= Date.now() / 1000) {
-        return true
-      }
-      else {
-        return false
-      };
-    } else {
-      return false
-    }
-  }
-
-  canLoad(): any {
-
-    const userData = this.authService.userData || ""
     if (userData.token) {
       const result = this.authService.refreshToken().toPromise().then(
         res => {
-          console.log("cloadRes", res)
           return true
         }, err => {
-          console.log("cloadErr", err)
           return false
         }
+      )
+      return result
+    } else if (!userData.token) {
+      return false
+    }  }
 
+  canLoad(): any {
+    const userData = this.authService.userData
+    if (userData.token) {
+      const result = this.authService.refreshToken().toPromise().then(
+        res => {
+          return true
+        }, err => {
+          return false
+        }
       )
       return result
     } else if (!userData.token) {
