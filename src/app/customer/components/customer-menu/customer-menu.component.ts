4a98983ed7d2d88ad2938c12e6d91d920b44385c
Mariano <mariano.b.fernandez@gmail.com>
2021-09-07 23:12:54 -0300

    add customer menu, adjust login

--- src/app/customer/components/customer-menu/customer-menu.component.ts
+++ src/app/customer/components/customer-menu/customer-menu.component.ts
@@ -1,13 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-customer-menu',
   templateUrl: './customer-menu.component.html',
   styleUrls: ['./customer-menu.component.css']
 })
-export class CustomerMenuComponent implements OnInit {
+export class CustomerMenuComponent implements OnInit, OnChanges {
+
+
+  @Input() openMenu = false;
+  @ViewChild('sidenav') public sidenav!: MatSidenav;
+
 
   constructor() { }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes)
+    this.sidenav.toggle();
+  }
 
   ngOnInit(): void {
   }
