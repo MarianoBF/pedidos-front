e85c64e91a2aeb41841b6b4ed7485857291e8734
Mariano <mariano.b.fernandez@gmail.com>
2021-09-18 14:22:33 -0300

    update orders dialogue to show details

--- src/app/admin/components/orders/orders-dialogue/orders-dialogue.component.ts
+++ src/app/admin/components/orders/orders-dialogue/orders-dialogue.component.ts
@@ -1,5 +1,6 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { OrdersService } from '../../../../services/orders.service';
 
 @Component({
   selector: 'app-orders-dialogue',
@@ -8,11 +9,16 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class OrdersDialogueComponent implements OnInit {
   status: string = "";
+  show = 'status'
 
   constructor(private dialogRef: MatDialogRef<OrdersDialogueComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: {id: number, status: string}) { }
+    @Inject(MAT_DIALOG_DATA) public data: {id: number, status: string, show?:string}, private ordersService: OrdersService) { }
 
   ngOnInit(): void {
+    console.log(this.data)
+    if (this.data.show === 'details') {
+      this.ordersService.getAllProductsForOrder(this.data.id).toPromise().then(res=>console.log(res))
+    }
   }
 
   handleSelect(status: any) {
