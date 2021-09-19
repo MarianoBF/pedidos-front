0f1fb6421682139d234325d504fadb83b348fdab
Mariano <mariano.b.fernandez@gmail.com>
2021-09-19 12:22:39 -0300

    add pagination & update sorting on admin orders

--- src/app/admin/components/orders/orders-list/orders-list.component.ts
+++ src/app/admin/components/orders/orders-list/orders-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { Sort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { switchMap, filter } from 'rxjs/operators';
 import { Order } from 'src/app/common/interfaces';
 import { OrdersService } from '../../../../services/orders.service';
@@ -14,40 +16,49 @@ import { OrdersDialogueComponent } from '../orders-dialogue/orders-dialogue.comp
 export class OrdersListComponent implements OnInit {
 
   displayedColumns: string[] = ['id_pedido', 'estado', 'hora', 'pago_monto', 'pago_via', 'id_usuario', 'details'];
-  dataSource: Order[] = [];
+  // dataSource: Order[] = [];
   currentStatus: string = "";
   modifyID: number = -1;
+  dataSource: any;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(private orderService: OrdersService, public dialog: MatDialog) {
-    this.orderService.getOrders().subscribe(data => this.dataSource = data);
+    this.orderService.getOrders().subscribe(data => {
+      this.dataSource =  new MatTableDataSource<Order>(data);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort
+    })
   }
 
   ngOnInit(): void {
   }
   
-  sortData(sort: Sort) {
-    const data = this.dataSource.slice();
-    if (!sort.active || sort.direction === '') {
-      this.dataSource = data;
-      return;
-    }
-    this.dataSource = data.sort((a, b) => {
-      const isAsc = sort.direction === 'asc';
-      switch (sort.active) {
-        case 'estado': return this.compare(a.estado, b.estado, isAsc);
-        case 'pago_via': return this.compare(a.pago_via, b.pago_via, isAsc);
-        case 'pago_monto': return this.compare(a.pago_monto, b.pago_monto, isAsc);
-        case 'hora': return this.compare(String(a.hora), String(b.hora), isAsc);
-        case 'id_usuario': return this.compare(a.id_usuario, b.id_usuario, isAsc);
-        case 'id_pedido': return this.compare(a.id_pedido!, b.id_pedido!, isAsc);
-        default: return 0;
-      }
-    });
-  }
+  // sortData(sort: Sort) {
+  //   console.log(this.dataSource)
+  //   const data = this.dataSource.slice();
+  //   if (!sort.active || sort.direction === '') {
+  //     this.dataSource = data;
+  //     return;
+  //   }
+  //   this.dataSource = data.sort((a:any, b:any) => {
+  //     const isAsc = sort.direction === 'asc';
+  //     switch (sort.active) {
+  //       case 'estado': return this.compare(a.estado, b.estado, isAsc);
+  //       case 'pago_via': return this.compare(a.pago_via, b.pago_via, isAsc);
+  //       case 'pago_monto': return this.compare(a.pago_monto, b.pago_monto, isAsc);
+  //       case 'hora': return this.compare(String(a.hora), String(b.hora), isAsc);
+  //       case 'id_usuario': return this.compare(a.id_usuario, b.id_usuario, isAsc);
+  //       case 'id_pedido': return this.compare(a.id_pedido!, b.id_pedido!, isAsc);
+  //       default: return 0;
+  //     }
+  //   });
+  // }
 
-  compare(a: number | string, b: number | string, isAsc: boolean) {
-    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
-  }
+  // compare(a: number | string, b: number | string, isAsc: boolean) {
+  //   return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+  // }
 
   showDetails(id:number) {
     const dialog = this.dialog.open(OrdersDialogueComponent, {
