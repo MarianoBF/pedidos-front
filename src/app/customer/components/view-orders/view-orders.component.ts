e69284a229ad9467705f81bb3981df44a980ffdd
Mariano <mariano.b.fernandez@gmail.com>
2021-09-24 08:35:06 -0300

    add sort/filter/paginator to customer orders

--- src/app/customer/components/view-orders/view-orders.component.ts
+++ src/app/customer/components/view-orders/view-orders.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { Order } from 'src/app/common/interfaces';
 import { OrdersService } from 'src/app/services/orders.service';
 
@@ -9,12 +12,23 @@ import { OrdersService } from 'src/app/services/orders.service';
 })
 export class ViewOrdersComponent {
 
-  displayedColumns: string[] = ['orderNumber', 'status', 'time', 'amount', 'type', 'user'];
-  dataSource: Order[] = [];
+  displayedColumns: string[] = ['id_pedido', 'estado', 'hora', 'pago_monto', 'pago_via'];
+  dataSource: any;
   currentStatus: string = "";
   modifyID: number = -1;
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
   constructor(private orderService: OrdersService) {
-    this.orderService.getOrdersForUser().subscribe(data => this.dataSource = data);
+    this.orderService.getOrdersForUser().subscribe(data => {
+      this.dataSource =  new MatTableDataSource<Order>(data);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort
+    })
+  }
+
+  filter = ($event: any) => {
+    this.dataSource.filter = $event.target.value.trim().toLocaleLowerCase();
   }
 }
