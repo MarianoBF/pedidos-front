799d562c1e54060d260b4f9753927c964577b673
Mariano <mariano.b.fernandez@gmail.com>
2021-09-25 10:22:03 -0300

    add loader to order list, adjust login

--- src/app/customer/components/view-orders/view-orders.component.ts
+++ src/app/customer/components/view-orders/view-orders.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
@@ -10,21 +10,31 @@ import { OrdersService } from 'src/app/services/orders.service';
   templateUrl: './view-orders.component.html',
   styleUrls: ['./view-orders.component.css']
 })
-export class ViewOrdersComponent {
+export class ViewOrdersComponent implements OnInit {
 
   displayedColumns: string[] = ['id_pedido', 'estado', 'hora', 'pago_monto', 'pago_via'];
   dataSource: any;
   currentStatus: string = "";
   modifyID: number = -1;
+  loading = true;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
   constructor(private orderService: OrdersService) {
+    this.getOrders();
+  }
+
+  ngOnInit() {
+    this.getOrders();
+  }
+
+  getOrders() {
     this.orderService.getOrdersForUser().subscribe(data => {
-      this.dataSource =  new MatTableDataSource<Order>(data);
+      this.dataSource = new MatTableDataSource<Order>(data);
       this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort
+      this.dataSource.sort = this.sort;
+      this.loading = false;
     })
   }
 
