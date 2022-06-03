f177b834d9191682a29bb20adc20f0b9c046ea64
Mariano <mariano.b.fernandez@gmail.com>
2022-06-03 09:32:10 -0300

    adjust btn, unsub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,29 +2,36 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from './services/auth.service';
 import { Router } from '@angular/router';
 import { loggedInUser } from './common/interfaces';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
   user?: loggedInUser = this.authService.userData;
   logged = false;
   role = "";
   openMenu = false;
   visitor = false;
+  subs: Subscription[] = [];
 
   constructor (private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
     // this.user = this.authService.userData;
-    this.authService.loggedStatus.subscribe(res=>this.logged = res);
-    this.authService.userInfo.subscribe(res=>{
+    this.subs.push(this.authService.loggedStatus.subscribe(res=>this.logged = res));
+    this.subs.push(this.authService.userInfo.subscribe(res=>{
       this.user = res;
-      this.role = res.role});
-    this.authService.visitor.subscribe(
-      res=>this.visitor = res)
+      this.role = res.role}));
+    this.subs.push(this.authService.visitor.subscribe(
+      res=>this.visitor = res));
+  }
+
+  ngOnDestroy() {
+    this.subs.forEach(sub => sub.unsubscribe());
   }
 
   logout() {
