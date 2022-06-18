0f6afdbd976636a02102fc4f1ee69769b0542750
Mariano <mariano.b.fernandez@gmail.com>
2022-06-18 15:26:39 -0300

    remove log & adjust

--- src/app/directives/image-alt.directive.ts
+++ src/app/directives/image-alt.directive.ts
@@ -9,7 +9,6 @@ export class ImageAltDirective {
     private viewContainer: ViewContainerRef) { }
 
   @Input() set appImageAlt(hasImage: any) {
-    console.log("a", hasImage)
     if (!hasImage) {
       this.viewContainer.clear();
     }
