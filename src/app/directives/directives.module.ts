6cbb0ea5bd17264001951e8f71ce188519015476
Mariano <mariano.b.fernandez@gmail.com>
2022-06-18 14:59:47 -0300

    custom structural directive

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PricelensDirective } from './pricelens.directive';
+import { ImageAltDirective } from './image-alt.directive';
 
 
 
 @NgModule({
   declarations: [
     PricelensDirective,
+    ImageAltDirective,
   ],
   imports: [
     CommonModule
   ],
   exports: [
     PricelensDirective,
+    ImageAltDirective,
   ]
 })
 export class DirectivesModule { }
---
+++ src/app/directives/image-alt.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[appImageAlt]'
+})
+export class ImageAltDirective {
+
+  constructor(private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef) { }
+
+  @Input() set appImageAlt(hasImage: any) {
+    console.log("a", hasImage)
+    if (!hasImage) {
+      this.viewContainer.clear();
+    }
+    else if (hasImage === 'null') {
+      this.viewContainer.clear();
+
+    } else {
+      this.viewContainer.createEmbeddedView(this.templateRef);
+    }
+  }
+
+}
