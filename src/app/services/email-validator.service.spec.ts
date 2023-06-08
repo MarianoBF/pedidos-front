dd587073501c5a51f6e09d2ee7a285673a6c26a0
Mariano <mariano.b.fernandez@gmail.com>
2023-06-08 18:22:56 -0300

    test for service

---
+++ src/app/services/email-validator.service.spec.ts
@@ -0,0 +1,68 @@
+import { HttpClient } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+import { AbstractControl } from '@angular/forms';
+import { of, timer } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { EmailValidatorService } from './email-validator.service';
+import { UsersService } from './users.service';
+
+describe('EmailValidatorService', () => {
+  let emailValidatorService: EmailValidatorService;
+  let usersService: UsersService;
+  let httpClient: HttpClient
+
+  beforeEach(async() => {
+    usersService = new UsersService(httpClient); // Create a mock instance of UsersService if needed
+    emailValidatorService = new EmailValidatorService();
+
+    await TestBed.configureTestingModule({
+        declarations: [],
+        imports: [],
+        providers: [
+          { provide: UsersService, useValue: usersService },
+        ]
+      }).compileComponents();  
+
+});
+
+  it('should return null if control is pristine', (done) => {
+    const control = { pristine: true } as AbstractControl;
+    const validatorFn = EmailValidatorService.validate(usersService);
+    const result$ = validatorFn(control);
+
+    result$.subscribe((result) => {
+      expect(result).toEqual(null);
+      done();
+    });
+
+  });
+
+  it('should return an Observable with emailTaken: true if email is already taken', (done) => {
+    const mockCheckMailResult = { available: 'no' }; // Mock the result of usersService.checkMail()
+    jest.spyOn(usersService, 'checkMail').mockReturnValue(of(mockCheckMailResult));
+
+    const control = { value: 'test@example.com' } as AbstractControl;
+    const validatorFn = EmailValidatorService.validate(usersService);
+    const result$ = validatorFn(control);
+
+    result$.subscribe((result) => {
+      expect(result).toEqual({ emailTaken: true });
+      done();
+    });
+  });
+
+  it('should return an Observable with null if email is available', (done) => {
+    const mockCheckMailResult = { available: 'yes' }; // Mock the result of usersService.checkMail()
+    jest.spyOn(usersService, 'checkMail').mockReturnValue(of(mockCheckMailResult));
+
+    const control = { value: 'test@test.com' } as AbstractControl;
+    const validatorFn = EmailValidatorService.validate(usersService);
+    const result$ = validatorFn(control);
+
+    result$.subscribe((result) => {
+      expect(result).toEqual(null);
+      done();
+    });
+  });
+
+});
